5c74cd60a0f8cf6c47511edb002b0f361f02b738
dreamc4t <eriksund1@gmail.com>
2023-08-31 19:37:55 +0200

    program ready

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using DndBackend.Models;
+using DndBackend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<MongoDBService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
