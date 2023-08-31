82e90899968c036682a76cf16bcbc7c6d145058f
dreamc4t <eriksund1@gmail.com>
2023-08-31 19:51:23 +0200

    id string not int

--- Models/Item.cs
+++ Models/Item.cs
@@ -2,11 +2,13 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Attributes;
 
+namespace DndBackend.Models;
+
 public class Item
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public int Id { get; set; }
+    public string? Id { get; set; }
 
     [BsonElement("name")]
     public string? Name { get; set; }
