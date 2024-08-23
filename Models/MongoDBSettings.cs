820907e901bbdfc37e4d6d455c47b0db91b3fac9
dreamc4t <eriksund1@gmail.com>
2024-08-23 09:14:56 +0200

    monguri from env

--- Models/MongoDBSettings.cs
+++ Models/MongoDBSettings.cs
@@ -2,7 +2,6 @@ namespace DndBackend.Models;
 
 public class MongoDBSettings
 {
-    public string ConnectionURI { get; set; } = null!;
     public string DatabaseName { get; set; } = null!;
     public string ItemsCollectionName { get; set; } = null!;
 
